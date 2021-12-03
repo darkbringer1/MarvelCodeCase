9391ea8eccba31286ebb4c82093a3b6c3983ae9a
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-12-03 13:11:39 +0300

    Initial Commit

---
+++ Marvel Code Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Code Case
+//
+//  Created by DarkBringer on 3.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Marvel Code CaseTests/Marvel_Code_CaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  Marvel_Code_CaseTests.swift
+//  Marvel Code CaseTests
+//
+//  Created by DarkBringer on 3.12.2021.
+//
+
+import XCTest
+@testable import Marvel_Code_Case
+
+class Marvel_Code_CaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
